ab9f616434daa7d12a3cbc9c7d5b51efb7167f59
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2015-11-18 20:11:43 +0100

    ignore file en database toegevoegd
    usernames en wachtwoorden zijn verboden om op te nemen in je source
    files. Anders kan namelijk iedereen inloggen en gegevens wijzigen of
    verwijderen.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
     
+    <?php include "templates/database.php" ?>
     <?php include "templates/head.php" ?>
 
     <body>
@@ -10,8 +11,22 @@
 
         <!-- content -->
         <h1>Welkom</h1>
+        <p>Dit is een paragraaf</p> 
 
-        <p>lipsum</p>
+        <p>Als de database connectie is gelukt dan staat er hieronder een lijstje met onze namen:</p>
+
+        <?php 
+        // php statement uit database
+        $statement = $pdo->prepare("SELECT * FROM testtabel");
+        $statement->execute();
+
+        while ($row = $statement->fetch()) {
+            $naam = $row["naam"];
+            $achternaam = $row["achternaam"];
+            print($naam . " " . $achternaam . "<br>");
+        }
+
+        ?>
 
         <?php include "templates/footer.php" ?>
         
