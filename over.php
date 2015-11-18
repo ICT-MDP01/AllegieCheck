32fc708c292d4d4bbc07438dfea8b4948d3b5152
jochem0125 <jochemschipper1@gmail.com>
2015-11-18 14:41:23 +0100

    over
    kleine tijdelijke tekst

--- over.php
+++ over.php
@@ -9,10 +9,14 @@
 
 
         <!-- content -->
-        <h1>Over</h1>
+        <h1>Over ons</h1>
 
-        <p>lipsum</p>
+        <p>Wij van Allergiecheck hebben deze site opgericht om het voor mensen met een allergie makkelijker te maken om uit eten te gaan.
+        In een oogomslag kunnen zij via de app checken waar
+        zij kunnen eten en wat zij daar precies kunnen eten. 
+         </p>
 
+        
         <?php include "templates/footer.php" ?>
         
     </body>
