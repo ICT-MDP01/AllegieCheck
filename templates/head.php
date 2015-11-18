763611b7211ef08beb4c572626b06baf1ca02aee
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2015-11-18 13:20:19 +0100

    basis templates gemaakt

---
+++ templates/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="utf-8">
+    <title>AllergieCheck</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- css links -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
