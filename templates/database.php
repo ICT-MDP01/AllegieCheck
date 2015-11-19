a98972a64ac34efeac9d8d76868d208c5372863d
Laurens zwaan <Hoodz@laurens.local>
2015-11-19 15:27:06 +0100

    restaurant pagina+style

--- templates/database.php
+++ templates/database.php
@@ -1,9 +1,15 @@
 <?php 
+​
+	$host = "db4free.net:3306/allergiecheck";
+    $database = "mysql:host=" . $host . ";dbname=allergiecheck;port=3306";
+    $user = "allergiecheck";
+    $pass = "sinterklaas";
+​
+    try {
+    	$pdo = new PDO($database, $user, $pass);
+    } catch(PDOException $exception) {
+	    echo $exception->getMessage();
+	}
 
-
-if (1==1) {
-  print ("hier komt de database");
-}
-
-
+​
 ?>
