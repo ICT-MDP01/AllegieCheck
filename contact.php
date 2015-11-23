5d20a267a3703cdd58f12da2a3399ccb9a414e0a
jochem0125 <jochemschipper1@gmail.com>
2015-11-23 15:47:10 +0100

    contact pagina 2.0 upgrade
    form geupgrade en pagina gestyld met css

--- contact.php
+++ contact.php
@@ -14,11 +14,28 @@
             <p>invultekst contactgegevens voor eigenaar site(moet nog komen)</p>
             
             <p>Laat hier uw mening achter of geef uw feedback</p>
+            <div class="bericht">
+            <p>Heb je vragen of kritiek? of wil je gewoon je mening achterlaten vul dan dit formulier in of stuur mij een email!</P>
+            </div>
+            
             
             <form method="get" action="contact.php">
-                <textarea name="comment"></textarea><br>
-                <input type="submit" name="Submit" value="Verstuur"><br>
+                 Naam: <input type="text" name="naam" value=""><br>   
+                 Email <input type="email" name="email" value=""><br>
+                 
+                 Bericht: <textarea name="comment"></textarea><br>
+                
+                 <input type="submit" name="Submit" value="Verstuur"><br>
+                
+               
+                
            </form>
+       <div class="afbeelding">
+        <img src="http://blog.trello.com/wp-content/uploads/2012/11/image49860831.png" alt="" height="250" width="350">
+       </div>
+
+
+
        </div>
               
         	
