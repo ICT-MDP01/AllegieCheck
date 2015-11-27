e6b2f2a327e6d7f42707f6b44438ac14d403efbd
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2015-11-27 14:43:30 +0100

    ftw

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// placeholder verwijderen on focus
+$('.searchText').focus(function(){
+   $(this).data('placeholder',$(this).attr('placeholder'))
+          .attr('placeholder','');
+}).blur(function(){
+   $(this).attr('placeholder',$(this).data('placeholder'));
+});
