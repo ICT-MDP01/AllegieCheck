97122ad54405f85d11af530e7ca643adc7afd60f
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2015-11-30 16:06:43 +0100

    mobiele navigatie

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-// placeholder verwijderen on focus
-$('.searchText').focus(function(){
-   $(this).data('placeholder',$(this).attr('placeholder'))
-          .attr('placeholder','');
-}).blur(function(){
-   $(this).attr('placeholder',$(this).data('placeholder'));
+$(document).ready(function() {
+
+	// placeholder verwijderen on focus
+	$('.searchText').focus(function(){
+	   $(this).data('placeholder',$(this).attr('placeholder'))
+	          .attr('placeholder','');
+	}).blur(function(){
+	   $(this).attr('placeholder',$(this).data('placeholder'));
+	});
+
+	// mobile navigation
+	$("#hamburger").click(function() {
+    	$("body").toggleClass("menuOpen");
+	});
+
+	$("#menuOpenOverlay").click(function() {
+    	$("body").toggleClass("menuOpen");
+	});
+
 });
+
