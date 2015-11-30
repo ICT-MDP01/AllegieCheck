3083ed3a2b1630036ca9155f666897f519914289
Elwin1010 <elstreekie@gmail.com>
2015-11-30 16:08:26 +0100

    commitment

--- restaurant.php
+++ restaurant.php
@@ -14,13 +14,32 @@ $pageName = "restaurant";
 
         <?php include "includes/header.php"; ?>
 
+            <?php
+
+                if (isset($_GET["id"])) {
+                    $restaurant_id = $_GET["id"];
+                } else {
+                    $restaurant_id = "Error";
+                }
+
+                // prepared statement
+                $stmt = $pdo->prepare(" SELECT * FROM restaurant r 
+                                        INNER JOIN restaurantsoort s 
+                                        ON r.soort_id = s.soort_id
+                                        WHERE restaurant_id='$restaurant_id'");
+                $stmt->execute();
+                $restaurant = $stmt->fetchAll();
+                $restaurant = $restaurant[0];
+
+            ?>
+
         <!-- content -->
         <main id="content">
-            <div class="banner">
+            <div class="banner" style="background-image: url(<?php print($restaurant["afbeelding"]); ?>);">
                 <div class="darken">
                     <div class="bannerTitle">
                         <div class="container">
-                            <h1>Restaurant</h1>
+                            <h1><?php print($restaurant["naam"]); ?></h1>
                             <p><a href="/restaurant.php#restaurantContact">contact</a></p>
                         </div>
                     </div>
@@ -31,8 +50,8 @@ $pageName = "restaurant";
 
                  <!-- info restaurant -->
                 <div class="restaurantInfo">
-                    <h3>Mexicaans restaurant (type)</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer blandit et odio in consequat. Aenean in blandit quam. Donec tincidunt rhoncus turpis sed semper. In at est et erat faucibus scelerisque. Maecenas nec tristique erat, nec maximus mauris. Praesent ultrices eros lectus, in posuere purus consectetur in.</p>
+                    <h3><?php print($restaurant["soort"]); ?></h3>
+                    <p><?php print($restaurant["informatie"]) ?></p>
                     <br>
                 </div> <!-- /.restaurantInfo -->
 
